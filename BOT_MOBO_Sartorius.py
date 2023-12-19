670064cef17d75f26f7c39da594ed834d0c45c10
Chang Jie Leong <changjie9@gmail.com>
2023-12-19 19:52:01 +0800

    Update BOT_MOBO_Sartorius.py
    simplify

--- BOT_MOBO_Sartorius.py
+++ BOT_MOBO_Sartorius.py
@@ -1,226 +1,11 @@
 # %%
-# basic dependencies
-
-import numpy as np
-from numpy import loadtxt
-from numpy import savetxt
-
-import pandas as pd
-import math
-import time
-from datetime import date
-
-np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
-
-###########
-
-# torch dependencies
-import torch
-
-tkwargs = {"dtype": torch.double, # set as double to minimize zero error for cholesky decomposition error
-           "device": torch.device("cuda:0" if torch.cuda.is_available() else "cpu")} # set tensors to GPU, if multiple GPUs please set cuda:x properly
-
-torch.set_printoptions(precision=3)
-
-###########
-
-# botorch dependencies
-import botorch
-
-# data related
-from botorch.utils.sampling import draw_sobol_samples
-from botorch.utils.transforms import unnormalize, normalize
-
-# surrogate model specific
-from botorch.models.gp_regression import SingleTaskGP
-from botorch.models.model_list_gp_regression import ModelListGP
-from botorch.models.transforms.outcome import Standardize
-from gpytorch.mlls.sum_marginal_log_likelihood import SumMarginalLogLikelihood
-from botorch import fit_gpytorch_model
-
-# qNEHVI specific
-from botorch.acquisition.multi_objective.objective import IdentityMCMultiOutputObjective
-from botorch.acquisition.multi_objective.monte_carlo import qNoisyExpectedHypervolumeImprovement
-
-# utilities
-from botorch.optim.optimize import optimize_acqf
-from botorch.sampling import SobolQMCNormalSampler
-from botorch.utils.multi_objective.pareto import is_non_dominated
-from botorch.utils.multi_objective.hypervolume import Hypervolume
-from typing import Optional
-from torch import Tensor
-from botorch.exceptions import BadInitialCandidatesWarning
-
-from gpytorch.constraints import GreaterThan
-from torch.optim import SGD
-from gpytorch.mlls import ExactMarginalLogLikelihood
-
-import warnings
-
-warnings.filterwarnings('ignore', category=BadInitialCandidatesWarning)
-warnings.filterwarnings('ignore', category=RuntimeWarning)
-
-# plotting dependencies
-import matplotlib
-from matplotlib import pyplot as plt
-%matplotlib inline
-
-SMALL_SIZE = 14
-MEDIUM_SIZE = 18
-BIGGER_SIZE = 20
-
-plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
-
-# %%
-class BO_LiqTransfer:
-
-    def __init__(self, liquid_name):
-        self.liquid_name = liquid_name
-        self._data = None
-        self.features = ['aspiration_rate','dispense_rate']
-        self.objectives = ['%error','time_asp_1000']
-        self.bmax = 1.25
-        self.bmin = 0.1
-        self._latest_suggestion = None
-        self._latest_volume = None
-        self._latest_acq_value = None
-        self.mean_volumes = [300,500,1000]
-    
-   
-    def set_data(self,df):
-        df['time_asp_1000'] = 1000/df['aspiration_rate'] + 1000/df['dispense_rate'] + df['delay_aspirate'] + df['delay_dispense']
-        if 'acq_value' not in df.columns:
-            df['acq_value'] = None
-
-        if df.loc[:,self.features].duplicated().sum()==0:
-            df_mean = df
-        else:
-            df_duplicates = df.where(df.duplicated(self.features,keep=False)==True).dropna(how='all')
-            df_incomplete = df.where(df.duplicated(self.features,keep=False)==False).dropna(how='all')
-            df_mean = pd.DataFrame(columns= df.columns)
-            for index,values in df_duplicates.drop_duplicates(self.features).iterrows():
-                if len(df_duplicates.loc[index:index+2]) == len(self.mean_volumes):
-                    mean_error =df_duplicates.loc[index:index+2,'%error'].abs().mean()
-                    df_duplicates.loc[index,'%error'] = -mean_error
-                    df_duplicates.loc[index, 'volume'] ='mean'+str(self.mean_volumes)
-                    df_mean = pd.concat([df_mean,df.loc[index:index+2],df_duplicates.loc[[index]]])
-                    
-                else:
-                    df_incomplete = pd.concat([df_incomplete,df_duplicates.loc[index:index+2]]).drop_duplicates()
-            df_mean = pd.concat([df_mean,df_incomplete])
-            df_mean = df_mean.reset_index(drop=True)    
-        self._data = df_mean
- 
-        
-
-
-
-    def data_from_csv(self,file_name):
-        data = pd.read_csv(file_name)
-        self.set_data(data)
-
-
-
-    def update_data(self,df):
-        self._latest_volume = df['volume'].iloc[-1]
-        updated_data = pd.concat([self._data,df],ignore_index=True)
-        self.set_data(updated_data)
-        return self._data
-                                
-
-    def xy_split(self):
-        df_train = self._data.where(self._data['volume']=='mean'+str(self.mean_volumes)).dropna(how='all')
-        x_train = df_train[self.features]
-        y_train = df_train[self.objectives]
-        return x_train,y_train
-
-    def set_bounds(self, x_train):
-        return torch.vstack([x_train[0]*self.bmin, x_train[0]*self.bmax])
-
-
-
-    def fit_surrogate(self):
-        x_train, y_train = self.xy_split()
-        x_train = torch.tensor(x_train.to_numpy(dtype=float), **tkwargs)
-        y_train = torch.tensor(y_train.to_numpy(dtype=float), **tkwargs)
-        y_train[:,0] = -torch.absolute(y_train[:,0])
-        y_train[:,1] = -torch.absolute(y_train[:,1])
-
-        problem_bounds = self.set_bounds(x_train)
-        time_upper = 1000/problem_bounds[0][0] +1000/problem_bounds[0][1] + 10
-        error_upper = y_train[:,0].abs().min()*1.25
-        ref_point = torch.tensor([-error_upper,-time_upper], **tkwargs)
-
-        train_x_gp = normalize(x_train, problem_bounds)
-        models = []
-        for i in range(y_train.shape[-1]):
-            models.append(SingleTaskGP(train_x_gp, y_train[..., i : i + 1], outcome_transform=Standardize(m=1)))
-        model1 = ModelListGP(*models)
-        mll1 = SumMarginalLogLikelihood(model1.likelihood, model1)
-
-        fit_gpytorch_model(mll1)
-    
-        return model1, ref_point, train_x_gp, problem_bounds
-    
-    def optimized_suggestions(self, random_state= 42):
-        if random_state != None:
-            torch.manual_seed(random_state) 
-        standard_bounds = torch.zeros(2, len(self.features), **tkwargs)
-        standard_bounds[1] = 1
-        model1, ref_point, train_x_gp, problem_bounds = self.fit_surrogate()
-        acq_func1 = qNoisyExpectedHypervolumeImprovement(
-        model=model1,
-        ref_point=ref_point, # for computing HV, must flip for BoTorch
-        X_baseline=train_x_gp, # feed total list of train_x for this current iteration
-        sampler=SobolQMCNormalSampler(sample_shape=512),  # determines how candidates are randomly proposed before selection
-        objective=IdentityMCMultiOutputObjective(outcomes=np.arange(len(self.objectives)).tolist()), # optimize first n_obj col 
-        prune_baseline=True, cache_pending=True)  # options for improving qNEHVI, keep these on
-        sobol1 = draw_sobol_samples(bounds=standard_bounds,n=512, q=1).squeeze(1)
-        sobol2 = draw_sobol_samples(bounds=standard_bounds,n=512, q=1).squeeze(1)
-        sobol_all = torch.vstack([sobol1, sobol2])
-            
-        acq_value_list = []
-        for i in range(0, sobol_all.shape[0]):
-            with torch.no_grad():
-                acq_value = acq_func1(sobol_all[i].unsqueeze(dim=0))
-                acq_value_list.append(acq_value.item())
-                
-        # filter the best 12 QMC candidates first
-        sorted_x = sobol_all.cpu().numpy()[np.argsort((acq_value_list))]
-        qnehvi_x = torch.tensor(sorted_x[-12:], **tkwargs)  
-        # unormalize our training inputs back to original problem bounds
-        new_x =  unnormalize(qnehvi_x, bounds=problem_bounds)
-        new_x = pd.DataFrame(new_x.numpy(),columns=['aspiration_rate','dispense_rate'])
-        new_x['acq_value'] = sorted(acq_value_list, reverse=True)[:12]
-        self._latest_suggestion = new_x[['aspiration_rate','dispense_rate']].iloc[0]
-        self._latest_acq_value = new_x['acq_value'].iloc[0]
-        return new_x
-        
-
-
-
-
-
-        
-    
-    
-# %%
 #Import relevant python packages
 import pandas as pd
 import time
 from matplotlib import pyplot as plt
-from pathlib import Path
-import sys
-REPOS = 'GitHub'
-ROOT = str(Path().absolute()).split(REPOS)[0]
-sys.path.append(f'{ROOT}{REPOS}')
+from init import BO_LiqTransfer
 
-from polylectric.configs.SynthesisB1 import SETUP, LAYOUT_FILE
+from configs.SynthesisB1 import SETUP, LAYOUT_FILE
 
 from controllably import load_deck      # optional
 load_deck(SETUP.setup, LAYOUT_FILE)     # optional
@@ -268,7 +53,7 @@ density = 0.8736
 liq = BO_LiqTransfer(liquid_name)
 # folder = (r"C:\Users\admin\Documents\GitHub\viscosity_liquid_transfer_Pablo\Sartorious_experiments\Initialisation_Data")
 liq.data_from_csv(liquid_name+'_BOTorch_exp3.csv')
-liq._data
+liq.data
 
 # %%
 # Change according to experiment
@@ -279,7 +64,7 @@ density = 0.8736
 liq = BO_LiqTransfer(liquid_name)
 folder = (r"C:\Users\admin\Documents\GitHub\viscosity_liquid_transfer_Pablo\Sartorious_experiments\Initialisation_Data")
 liq.data_from_csv(folder+r'/'+'Initialisation_'+std+'.csv')
-liq._data
+liq.data
 
 # %%
 liq.optimized_suggestions()
@@ -417,11 +202,11 @@ df.iloc[-1,-2]= 'Complete Transfer'
 df.iloc[-1,-1]= 'Blowout before'
 
 # %%
-liq.update_data(df) 
+liq.update_data(df=df) 
 
 # %%
 #save after each standard-experiment iteration
-liq._data.to_csv(liquid_name+'_BOTorch_exp3.csv', index = False)
+liq.data.to_csv(liquid_name+'_BOTorch_exp3.csv', index = False)
 
 # %%
 pipette.ejectTipAt(bin.wells['A1'].top)
@@ -435,7 +220,7 @@ pipette.mover.connect()
 
 # %%
 
-liq._data
+liq.data
 
 # %%
 pipette.liquid.dispense(1000)
@@ -457,4 +242,4 @@ density = 0.8736
 liq = BO_LiqTransfer(liquid_name)
 folder = (r"C:\Users\admin\Documents\GitHub\viscosity_liquid_transfer_Pablo\Sartorious_experiments\Initialisation_Data")
 liq.data_from_csv(liquid_name+'_BOTorch_exp3.csv')
-liq._data
+liq.data
